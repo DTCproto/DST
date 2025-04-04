8902c8876089c18ff600f96eb2b2650d87beee1c
DTCproto <61973955+DTCproto@users.noreply.github.com>
2025-04-05 07:25:21 +0800

    update version

--- ext/Ad/TengxunCloudAd2.js
+++ ext/Ad/TengxunCloudAd2.js
@@ -1,9 +1,13 @@
-import {Image} from "antd";
-import React from "react";
-
-export default ()=>{
-    return(
-        <>
-        </>
-    )
-}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+var antd_1 = require("antd");
+var react_1 = __importDefault(require("react"));
+exports.default = (function () {
+    return ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, {
+        children: ""
+    }));
+});
